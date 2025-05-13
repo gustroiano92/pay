667bfbc08dc6399e11d15118971c49b7a23b590b
gustroiano92 <gustavo@protectsite.com.br>
2025-05-13 19:55:18 -0300

    Fix path dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ module.exports = {
     mode: 'development',
     entry: './src/main.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public/dist'),
         filename: 'bundle.js',
-        publicPath: '/'
+        publicPath: '/dist/'
     },
     devServer: {
         static: {
